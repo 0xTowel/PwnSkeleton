350338b419d9cb2f557021c04ac0071a729556c6
0xTowel <towel@the2702.com>
2018-11-06 02:43:08 -0500

    add basic pwn skeleton

---
+++ solve.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+from pwn import *
+import click
+
+####################
+# Challenge Server #
+####################
+
+SERVER = "the.server.goes.here"
+PORT = 1337
+
+
+def exploit(p):
+    # Payload start...
+    payload = b'A' * 64  # Padding
+    payload += pop_rdx
+
+    # Pwn here...
+
+    assert len(payload) <= 2702  # Max payload size in the challenge
+    p.recvuntil(b':')
+    p.sendline(payload)
+    p.interactive()
+
+
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.argument(
+    "binary",
+    required=False,
+    default=None,
+    type=click.Path(exists=True)
+)
+@click.option(
+    "-np", "--no-pause",
+    is_flag=True,
+    default=False,
+    type=click.BOOL,
+    help=("Do not pause the program when started. "
+          "Off by default, ignored if connecting to a server.")
+)
+def start_exploit(binary, no_pause):
+    """
+    Connect to the server or binary and call the exploit.
+
+    If given a local binary to exploit, run the exploit locally.
+    Otherwise, connect to a remote server.
+    """
+    p = process([binary]) if binary else remote(SERVER, PORT)
+
+    # Pause the process and show the PID giving time for a debugger to attach
+    if not no_pause and binary:
+        click.echo(f"[+] PID: {util.proc.pidof(p)}")
+        pause()
+
+    exploit(p)
+    print("[*] Exiting...")
+    p.kill() if binary else p.close()
+
+
+if __name__ == "__main__":
+    start_exploit()
